3df2af4d895cc493d97694c2a6e0f9a54c0526f9
Junian Triajianto <author@junian.net>
2019-02-26 19:04:20 +0700

    Add basic mainviewmodel

---
+++ src/InstantCalculator/ViewModels/MainViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows.Input;
+using Juniansoft.MvvmReady;
+
+namespace InstantCalculator.ViewModels
+{
+    public class MainViewModel: ViewModelBase
+    {
+        Jint.Engine _js;
+
+        public MainViewModel()
+        {
+            _js = new Jint.Engine();
+        }
+
+        private string _mathExperession;
+        public string MathExpression
+        {
+            get => _mathExperession;
+            set => Set(ref _mathExperession, value);
+        }
+
+        private string _mathResult;
+        public string MathResult
+        {
+            get => _mathResult;
+            set => Set(ref _mathResult, value);
+        }
+
+        public double? Calculate(string mathExpression)
+        {
+            try
+            {
+                var result = _js.Execute(mathExpression)
+                                .GetCompletionValue()
+                                .ToString();
+
+                if (double.TryParse(result, out var dec))
+                    return dec;
+            }
+            catch { }
+            return null;
+        }
+
+        private ICommand _calculateCommand;
+        public ICommand CalculateCommand => _calculateCommand ??
+            (_calculateCommand = new Command(
+            () => 
+            {
+                var result = Calculate(MathExpression);
+                MathResult = result == null ? "N/A" : result.ToString();
+            }));
+    }
+}
