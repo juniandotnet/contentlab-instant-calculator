8df9fcf6f7c8e46b4d684533785afda240e4cf2b
Junian Triajianto <author@junian.net>
2019-02-27 15:10:34 +0700

    Add basic unit test

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -14,4 +14,4 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Add ability to display calculation result.
 - Add ability to record calculation history.
 
-[Unreleased]: https://github.com/junian/instant-calculator/compare/HEAD...HEAD
+[Unreleased]: https://github.com/junian/instant-calculator/compare/v0.0.1...HEAD
---
+++ src/InstantCalculator.Tests/ViewModels/MainViewModelExperiment.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using InstantCalculator.ViewModels;
+using Xunit;
+
+namespace InstantCalculator.Tests.ViewModels
+{
+    public class MainViewModelExperiment
+    {
+        MainViewModel _vm;
+
+        public MainViewModelExperiment()
+        {
+            _vm = new MainViewModel();
+        }
+
+        [Theory]
+        [InlineData("1+1", 2)]
+        [InlineData("1.5+1", 2.5)]
+        [InlineData("2+2", 4)]
+        [InlineData("4-1", 3)]
+        [InlineData("9*8*7", 504)]
+        [InlineData("woah", null)]
+        [InlineData("1+2-", null)]
+        [InlineData("1+2+'a'", null)]
+        public void StringCalculationTest(string mathExpression, double? expected)
+        {
+            var actual = _vm.Calculate(mathExpression);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
